44c117d37ea13a83e2182fb210a9e1f99fd1a935
Black Jack <th.lennymero@gmail.com>
2016-07-22 00:00:41 +0700

    Change Content View
    re-json structure

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -51,7 +51,7 @@ angular.module('starter.controllers', [])
           var x2js = new X2JS();
           var jsonData = x2js.xml_str2json(data);
           sources = jsonData;
-          console.log(sources);
+          $localStorage.jsonData_en = jsonData;
           $scope.playlists = sources.rss.channel.item;
           $scope.getImgPath = function(index){
             $scope.encoded = $scope.playlists[index].encoded.__cdata;
@@ -72,6 +72,7 @@ angular.module('starter.controllers', [])
           var jsonDatakorea = x2js.xml_str2json(datakorea);
           korea = jsonDatakorea;
           $scope.playlistskorea = korea.rss.channel.item;
+          $localStorage.jsonDatakorea = jsonDatakorea;
           $scope.getImgPathkorea = function(index){
             $scope.encoded = $scope.playlistskorea[index].encoded.__cdata;
             $scope.headParam = $scope.encoded.search("uploads/");
@@ -91,6 +92,7 @@ angular.module('starter.controllers', [])
           var jsonDatajapan = x2js.xml_str2json(datajapan);
           japan = jsonDatajapan;
           $scope.playlistsjapan = japan.rss.channel.item;
+          $localStorage.jsonDatajapan = jsonDatajapan;
           $scope.getImgPathjapan = function(index){
             $scope.encoded = $scope.playlistsjapan[index].encoded.__cdata;
             $scope.headParam = $scope.encoded.search("uploads/");
@@ -109,6 +111,7 @@ angular.module('starter.controllers', [])
           var x2js = new X2JS();
           var jsondatachina = x2js.xml_str2json(datachina);
           china = jsondatachina;
+          $localStorage.jsonDatachina = jsondatachina;
           $scope.playlistschina = china.rss.channel.item;
           $scope.getImgPathchina = function(index){
             $scope.encoded = $scope.playlistschina[index].encoded.__cdata;
@@ -148,56 +151,53 @@ angular.module('starter.controllers', [])
 })
 
 .controller('ContentCtrl', function($scope,$stateParams,$http,$sce,$stateParams,$state,$localStorage,$ionicHistory,$timeout,$ionicLoading,$ionicNavBarDelegate) {
-    document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady(){screen.unlockOrientation();}
+
+  $scope.$on('$ionicView.enter', function(e) {
+      $ionicNavBarDelegate.showBar(true);
+      $ionicLoading.show({
+          content: 'Fetching Data',
+          animation: 'fade-in',
+          showBackdrop: true,
+          maxWidth: 200,
+          showDelay: 0
+      });
+  });
+  $scope.$on('$ionicView.afterEnter', function(){
     $scope.cate_id = $stateParams.cate_id;
     $scope.index_id = $stateParams.index;
     $scope.cuts = $stateParams.cuts;
-    $scope.loadSeries = function(uristr,index_id,cuts){
-          $scope.jsonvideodata = uristr;
-          $ionicLoading.hide();
-          //console.log(JSON.stringify($scope.jsonvideodata));
-          $scope.contentdata = $scope.jsonvideodata.rss.channel.item;
-          $scope.enclosure_list = $scope.contentdata[index_id].enclosure;
-          $scope.enclosure_list_length = $scope.contentdata[index_id].enclosure.length;
-          $scope.title = $scope.contentdata[index_id].title;
-          $scope.MediaFiles = $scope.contentdata[index_id].enclosure[cuts]._url;
-          $scope.mediaType = $scope.contentdata[index_id].enclosure[cuts]._type;
-          console.log($scope.MediaFiles);
-          $scope.getImgPathcontent = function(index){
-            $scope.encoded = $scope.contentdata[index_id].encoded.__cdata;
-            $scope.headParam = $scope.encoded.search("uploads/");
-            $scope.TailParam = $scope.encoded.search('" alt');
-            $scope.middleParams = "http://www.seriesubthai.co/wp-content/"+$scope.encoded.substring($scope.headParam, $scope.TailParam);
-            return $scope.middleParams;
-          }
+    if($scope.cate_id == 1){
+      $scope.uri = "http://www.seriesubthai.co/category/usa-series/feed/";
+    }else if($scope.cate_id == 2){
+      $scope.uri = "http://www.seriesubthai.co/category/korea-series/feed/";
+    }else if($scope.cate_id == 3){
+      $scope.uri = "http://www.seriesubthai.co/category/japanese-series/feed/";
+    }else{
+      $scope.uri = "http://www.seriesubthai.co/category/chinese-series/feed/";
+    }
+    $http.get($scope.uri).success(function (datajapan) {
+      $ionicLoading.hide();
+        var x2js = new X2JS();
+        var jsonDatajapan = x2js.xml_str2json(datajapan);
+        japan = jsonDatajapan;
+        console.log(JSON.stringify(jsonDatajapan));
+        $scope.playlistsjapan = jsonDatajapan.rss.channel.item;
+        $scope.contentdata = jsonDatajapan.rss.channel.item;
+        $scope.enclosure_list = $scope.contentdata[$scope.index_id].enclosure;
+        $scope.enclosure_list_length = $scope.contentdata[$scope.index_id].enclosure.length;
+        $scope.title = $scope.contentdata[$scope.index_id].title;
+        $scope.MediaFiles = $scope.contentdata[$scope.index_id].enclosure[$scope.cuts]._url;
+        $scope.mediaType = $scope.contentdata[$scope.index_id].enclosure[$scope.cuts]._type;
+        console.log($scope.MediaFiles);
+        $scope.getImgPathcontent = function(index){
+          $scope.encoded = $scope.contentdata[$scope.index_id].encoded.__cdata;
+          $scope.headParam = $scope.encoded.search("uploads/");
+          $scope.TailParam = $scope.encoded.search('" alt');
+          $scope.middleParams = "http://www.seriesubthai.co/wp-content/"+$scope.encoded.substring($scope.headParam, $scope.TailParam);
+          return $scope.middleParams;
+        }
 
-    };
+    });
+  });
 
-    $scope.$on('$ionicView.enter', function(e) {
-        $ionicNavBarDelegate.showBar(true);
-        $ionicLoading.show({
-            content: 'Fetching Data',
-            animation: 'fade-in',
-            showBackdrop: true,
-            maxWidth: 200,
-            showDelay: 0
-        });
-    });
-    $scope.$on('$ionicView.afterEnter', function(){
-      setTimeout( function() {
-            $ionicLoading.hide();
-      }, 1000);
-      if($scope.cate_id == 1){
-        $scope.uri = $localStorage.jsonData_en;
-      }else if($scope.cate_id == 2){
-        $scope.uri = $localStorage.jsonDatakorea;
-      }else if($scope.cate_id == 3){
-        $scope.uri = $localStorage.jsonDatajapan;
-      }else{
-        $scope.uri = $localStorage.jsonDatachina;
-      }
-      $scope.loadSeries($scope.uri,$scope.index_id,$scope.cuts);
-      console.log("LoadSeries:",$scope.uri,$scope.index_id,$scope.cuts);
-    });
 });
